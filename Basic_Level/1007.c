0476986165642055f126b8d2272305127593fb26
Bachzart <whistlesilp@gmail.com>
2019-06-25 21:04:55 +0800

    1007

---
+++ Basic_Level/1007.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <math.h>
+typedef enum{ false, true} bool;
+
+bool IsPrime(int n);
+
+int main(int argc, char const *argv[]) {
+	int i, N, count = 0;
+	scanf("%d", &N);
+	for(i = 2; i <= N - 2; i++) {
+		if(IsPrime(i) && IsPrime(i + 2)) {
+			count++;
+		}
+	}
+	printf("%d\n", count);
+	return 0;
+}
+
+bool IsPrime(int n) {
+	bool flag = true;
+	if(n <= 1 || n % 2 == 0) {
+		flag = false;
+	} else {
+		int i;
+		for(i = 3; i <= (int)sqrt(n); i+=2) {
+			if(n % i == 0) {
+				flag = false;
+				break;
+			}
+		}
+	} 
+	return flag;
+}
