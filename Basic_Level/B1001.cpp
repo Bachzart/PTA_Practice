51a1563f6310b57cb79e27db3565d6eee05150b0
Bachzart <whistlesilp@gmail.com>
2019-07-12 19:20:23 +0800

    update

--- Basic_Level/B1001.cpp
+++ Basic_Level/B1001.cpp
@@ -1,18 +1,13 @@
 #include <stdio.h>
 
-int main(int argc, char const *argv[])
-{
-	int n, count=0;
+int main(int argc, char const *argv[]) {
+	int n, count = 0;
 	scanf("%d", &n);
-	while(1)
-	{
-		if(n == 1) break;
-		if(n%2 == 0){
-			n/=2;
-		}else{
-			n=3*n+1;
-			n/=2;
+	while(n != 1) {
+		if(n % 2 == 1) {
+			n = 3 * n + 1;
 		}
+		n /= 2;
 		count++;
 	}
 	printf("%d\n", count);
