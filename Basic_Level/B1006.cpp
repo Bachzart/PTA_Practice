5d5e7a1384f5261e4a0c5d87a0edf45ba9e9e73e
Bachzart <whistlesilp@gmail.com>
2019-07-13 23:02:43 +0800

    update

--- Basic_Level/B1006.cpp
+++ Basic_Level/B1006.cpp
@@ -1,25 +1,19 @@
-#include <stdio.h>
+#include <cstdio>
 
-int main(int argc, char const *argv[])
-{
+int main(int argc, char const *argv[]) {
 	int i, num, unit, tens, hundred;
 	scanf("%d", &num);
 	unit = num % 10;
 	tens = num / 10 % 10;
 	hundred = num / 100;
-	// printf("unit=%d, tens=%d, hundred=%d\n", unit, tens, hundred);
-	for(i=0; i<hundred; i++)
-	{
+	for(i = 0; i < hundred; i++) {
 		printf("B");
 	}
-	for(i=0; i<tens; i++)
-	{
+	for(i = 0; i < tens; i++) {
 		printf("S");
 	}
-	for(i=1; i<=unit; i++)
-	{
+	for(i = 1; i <= unit; i++) {
 		printf("%d", i);
 	}
-	printf("\n");
 	return 0;
 }
