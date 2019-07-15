2cc17a85367a169d1e6f0c2d7c4a5f88b7c93d01
Bachzart <whistlesilp@gmail.com>
2019-07-15 23:03:10 +0800

    07-15-2019

---
+++ Basic_Level/B1030.cpp
@@ -0,0 +1,37 @@
+#include <cstdio>
+#include <algorithm>
+using namespace std; 
+const int MAXN = 100000 + 10;
+int n, p, Num[MAXN];
+int BinarySearch(int i, long long x);
+
+int main(int argc, char const *argv[]) {
+	scanf("%d %d", &n, &p);
+	for(int i = 0; i < n; i++) {
+		scanf("%d", &Num[i]);
+	}
+	sort(Num, Num + n);
+	int ans = 1;
+	for(int i = 0; i < n; i++) {
+		int j = BinarySearch(i, (long long)Num[i] * p);
+		ans = max(ans, j - i);
+	}
+	printf("%d", ans);
+	return 0;
+}
+
+int BinarySearch(int i, long long x) {
+	if(Num[n - 1] <= x) {
+		return n;
+	}
+	int left = i + 1, right = n - 1, mid;
+	while(left < right) {
+		mid = (left + right) / 2;
+		if(Num[mid] <= x) {
+			left = mid + 1;
+		} else {
+			right = mid;
+		}
+	}
+	return left;
+}
