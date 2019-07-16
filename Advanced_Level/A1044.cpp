504f472358f2b8f5bd48760d41aae2d51c195e89
Bachzart <whistlesilp@gmail.com>
2019-07-16 22:20:12 +0800

    07-16-2019

---
+++ Advanced_Level/A1044.cpp
@@ -0,0 +1,44 @@
+#include <cstdio>
+const int MAXN = 100000 + 10;
+int sum[MAXN];
+int n, S, nearS = 100000000 + 10;
+
+int UpperBound(int L, int R, int x);
+
+int main(int argc, char const *argv[]) {
+	scanf("%d %d", &n, &S);
+	sum[0] = 0;
+	for(int i = 1; i <= n; i++) {
+		scanf("%d", &sum[i]);
+		sum[i] += sum[i - 1];
+	}
+	for(int i = 1; i <= n; i++) {
+		int j = UpperBound(i, n + 1, sum[i - 1] + S);
+		if(sum[j - 1] - sum[i - 1] == S) {
+			nearS = S;
+			break;
+		} else if(j <= n && sum[j] - sum[i - 1] < nearS) {
+			nearS = sum[j] - sum[i - 1];
+		}
+	}
+	for(int i = 1; i <= n; i++) {
+		int j = UpperBound(i, n + 1, sum[i - 1] + nearS);
+		if(sum[j - 1] - sum[i - 1] == nearS) {
+			printf("%d-%d\n", i, j - 1);
+		}
+	}
+	return 0;
+}
+
+int UpperBound(int L, int R, int x) {
+	int left = L, right = R, mid;
+	while(left < right) {
+		mid = (left + right) / 2;
+		if(sum[mid] > x) {
+			right = mid;
+		} else {
+			left = mid + 1;
+		}
+	}
+	return left;
+}
