8adb6e5df4ae45f5a10e397aef71bacd2eef6c5b
Bachzart <whistlesilp@gmail.com>
2019-07-16 22:19:17 +0800

    new solutios

--- Advanced_Level/A1048.cpp
+++ Advanced_Level/A1048.cpp
@@ -1,3 +1,51 @@
+/* method 1: use dichotomy
+*/
+#include <cstdio>
+#include <algorithm>
+using namespace std;
+const int MAXN = 100000 + 10;
+int coins[MAXN];
+
+int BinarySearch(int *array, int left, int right, int x);
+
+int main(int argc, char const *argv[]) {
+	int N, M;
+	scanf("%d %d", &N, &M);
+	for(int i = 1; i <= N; i++) {
+		scanf("%d", &coins[i]);
+	}
+	sort(coins + 1, coins + N + 1);
+	int i;
+	for(i = 1; i <= N; i++) {
+		int pos = BinarySearch(coins, 1, N, M - coins[i]);
+		if(pos != -1 && pos != i) {
+			printf("%d %d\n", coins[i], coins[pos]);
+			break;
+		}
+	}
+	if(i - 1 == N) {
+		printf("No Solution\n");
+	}
+	return 0;
+}
+
+int BinarySearch(int *array, int left, int right, int x) {
+	int mid;
+	while(left <= right) {
+		mid = (left + right) / 2;
+		if(array[mid] == x) {
+			return mid;
+		} else if(array[mid] > x) {
+			right = mid - 1;
+		} else {
+			left = mid + 1;
+		}
+	}
+	return -1;
+}
+
+/* method 2: use hash
+
 #include <cstdio>
 #include <algorithm>
 using namespace std;
@@ -31,3 +79,5 @@ int main(int argc, char const *argv[]) {
 	}
 	return 0;
 } 
+
+*/
