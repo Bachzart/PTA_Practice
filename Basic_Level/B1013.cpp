c9a684423b8bd8f97900abbe5179960d87ff5869
Bachzart <whistlesilp@gmail.com>
2019-07-24 21:43:39 +0800

    add new solutions

--- Basic_Level/B1013.cpp
+++ Basic_Level/B1013.cpp
@@ -1,3 +1,35 @@
+// method 1: use Eratosthenes
+#include <cstdio>
+const int MAXN = 1000001;
+int prime[MAXN], pNum = 0;
+bool p[MAXN] = {false};
+
+void filterPrime(int n) {
+	for(int i = 2; i < MAXN; i++) {
+		if(p[i] == false) {
+			prime[pNum++] = i;
+			if(pNum >= n) break;
+			for(int j = i + i; j < MAXN; j += i) {
+				p[j] = true;
+			}
+		}
+	}
+}
+
+int main(int argc, char const *argv[]) {
+	int m, n, count = 0;
+	scanf("%d %d", &m, &n);
+	filterPrime(n);
+	for(int i = m; i <= n; i++) {
+		printf("%d", prime[i - 1]);
+		count++;
+		if(count % 10 != 0 && i < n) putchar(' ');
+		else putchar('\n');
+	}
+	return 0;
+}
+
+/* method 2: normal ways
 #include <cstdio>
 #include <cmath>
 #define MAX 15000
@@ -43,3 +75,4 @@ bool IsPrime(int number) {
 	}
 	return flag;
 }
+*/
