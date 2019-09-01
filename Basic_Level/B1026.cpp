f1bd364f84cd6ec747b04af3e45e707dbdf9ecf6
Bachzart <whistlesilp@gmail.com>
2019-09-01 23:10:43 +0800

    refactor

--- Basic_Level/B1026.cpp
+++ Basic_Level/B1026.cpp
@@ -1,21 +1,9 @@
-#include <stdio.h>
-#define CLK_TCK 100
-
-int main(int argc, char const *argv[])
-{
-	int c1, c2, seconds, minutes, hours;
-	scanf("%d %d", &c1, &c2);
-	seconds = (c2 - c1 + 50) / CLK_TCK;	//c2 - c1 + 50 -> round
-	/*
-	hours = seconds / 3600;
-	seconds -= hours*3600;
-	minutes = seconds / 60;
-	seconds -= minutes*60;
-	*/
-	hours = seconds / 3600;
-	seconds = seconds % 3600;
-	minutes = seconds / 60;
-	seconds = seconds % 60;
-	printf("%02d:%02d:%02d\n", hours, minutes, seconds);
-	return 0;
+#include <iostream>
+using namespace std;
+int main() {
+    int c1, c2, c;
+    scanf("%d %d", &c1, &c2);
+    c = (c2 - c1 + 50) / 100;
+    printf("%02d:%02d:%02d", c / 3600, c / 60 % 60, c % 60);
+    return 0;
 }
