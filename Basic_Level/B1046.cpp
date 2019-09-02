9901c96cb617ac9bef73793e8efe5346076f5a40
Bachzart <whistlesilp@gmail.com>
2019-09-02 22:57:34 +0800

    refactor: simplification

--- Basic_Level/B1046.cpp
+++ Basic_Level/B1046.cpp
@@ -1,19 +1,16 @@
-#include <stdio.h>
+#include <iostream>
+using namespace std;
 
-int main(int argc, char const *argv[]) {
-	int i, N, A_Count, A_Voice, A_Write, B_Count, B_Voice, B_Write, Sum_Voice;
-	A_Count = B_Count = Sum_Voice = 0;
-	scanf("%d", &N);
-	for(i = 0; i < N; i++) {
-		scanf("%d %d %d %d", &A_Voice, &A_Write, &B_Voice, &B_Write);
-		Sum_Voice = A_Voice + B_Voice;
-		if(A_Write == Sum_Voice && B_Write != Sum_Voice) {
-			B_Count++;
-		}
-		if(B_Write == Sum_Voice && A_Write != Sum_Voice) {
-			A_Count++;
-		}
-	}
-	printf("%d %d\n", A_Count, B_Count);
-	return 0;
+int main() {
+    int n, a, b, c, d, countA = 0, countB = 0;
+    cin >> n;
+    while(n--) {
+        cin >> a >> b >> c >> d;
+        if(a + c == b && a + c == d) continue;	// all win
+        else if(a + c != b && a + c != d) continue;	// all lose
+        else if(a + c == b) countB++;	// A win
+        else countA++;	// B win
+    }
+    cout << countA << ' ' << countB;
+    return 0;
 }
