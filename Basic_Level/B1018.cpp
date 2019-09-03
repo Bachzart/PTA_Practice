841b577ec5eae2f4bfa1897c38d0b42b88647fa1
Bachzart <whistlesilp@gmail.com>
2019-09-03 22:53:07 +0800

    refactor: simplified

--- Basic_Level/B1018.cpp
+++ Basic_Level/B1018.cpp
@@ -1,68 +1,49 @@
-#include <stdio.h>
-/*
-In the count array:
-0 -> victory
-1 -> dogfall
-2 -> defeat
-
-In the gesture array:
-0 -> Bu
-1 -> Chuizi
-2 -> Jiandao
-(Laugh~)
-*/
-char Gesture[4] = {'B', 'C', 'J'};
-int MaxOfArray(int *array);
-
-int main(int argc, char const *argv[]) {
-	int temp, N, A_Count[3] = {0}, A_Gesture[3] = {0}, B_Gesture[3] = {0};
-	char A, B;
-	scanf("%d%*c", &N);
-	while(N--) {
-		scanf("%c %c%*c", &A, &B);
-		if(A == B) {
-			A_Count[1]++;	//dogfall
-		} else {
-			if(A == 'C') {
-				if(B == 'J') {
-					A_Count[0]++;	//A win
-					A_Gesture[1]++;	//Chuizi
-				} else {
-					A_Count[2]++;	//B win
-					B_Gesture[0]++;	//Bu
-				}
-			} else if(A == 'B') {
-				if(B == 'C') {
-					A_Count[0]++;	//A win
-					A_Gesture[0]++;	//Bu
-				} else {
-					A_Count[2]++;	//B win
-					B_Gesture[2]++;	//Jiandao
-				}
-			} else if(A == 'J') {
-				if(B == 'B') {
-					A_Count[0]++;	//A win
-					A_Gesture[2]++;	//Jiandao
-				} else {
-					A_Count[2]++;	//B win
-					B_Gesture[1]++;	//Chuizi
-				}
-			}
-		}
-	}
-	printf("%d %d %d\n", A_Count[0], A_Count[1], A_Count[2]);	//For A
-	printf("%d %d %d\n", A_Count[2], A_Count[1], A_Count[0]);	//For B
-	printf("%c %c\n", Gesture[MaxOfArray(A_Gesture)], Gesture[MaxOfArray(B_Gesture)]);
-	return 0;
+#include <iostream>
+using namespace std;
+char gesture(int *a) {
+    int index = 1;
+    char ges = '\0';
+    for(int i = 1; i <= 3; i++) {
+        if(a[i] > a[index]) index = i;
+    }
+    switch(index) {
+        case 1: ges = 'B'; break;
+        case 2: ges = 'C'; break;
+        case 3: ges = 'J'; break;
+    }
+    return ges;
 }
-
-int MaxOfArray(int *array) {
-	int i, max = -1, index = -1;
-	for(i = 0; i < 3; i++) {
-		if(array[i] > max) {
-			max = array[i];
-			index = i;
-		}
-	}
-	return index;
+int main() {
+    int n, tie = 0, win = 0, ar[4] = {0}, br[4] = {0};
+    cin >> n;
+    char a, b;
+    for(int i = 1; i <= n; i++) {
+        cin >> a >> b;
+        if(a == b) tie++;
+        else if(a == 'C' && b == 'J') {
+            win++;
+            ar[2]++;
+        }
+        else if(a == 'J' && b == 'B') {
+            win++;
+            ar[3]++;
+        }
+        else if(a == 'B' && b == 'C') {
+            win++;
+            ar[1]++;
+        }
+        else if(b == 'C' && a == 'J') {
+            br[2]++;
+        }
+        else if(b == 'J' && a == 'B') {
+            br[3]++;
+        }
+        else if(b == 'B' && a == 'C') {
+            br[1]++;
+        }
+    }
+    cout << win << ' ' << tie << ' ' << n - tie - win << endl;
+    cout << n - tie - win << ' ' << tie << ' ' << win << endl;
+    cout << gesture(ar) << ' ' << gesture(br);
+    return 0;
 }
