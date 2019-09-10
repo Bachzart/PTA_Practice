0a214e36484653be0223aa49ad211fc03f6957b0
Bachzart <whistlesilp@gmail.com>
2019-09-10 23:04:33 +0800

    refactor: simplification

--- 201909_MOOC_DS_ZhouQiang/example1.cpp
+++ 201909_MOOC_DS_ZhouQiang/example1.cpp
@@ -4,7 +4,7 @@ int X[4] = {-1, 1, 0, 0};
 int Y[4] = {0, 0, -1, 1};
 int m, n, area[25][25];
 bool outofrange(int x, int y) {
-	if(x < 0 || y < 0 || x > m - 1 || y > n - 1 || area[x][y] == 3) return 1;
+	if(x < 0 || y < 0 || x > m - 1 || y > n - 1) return 1;
 	return 0;
 }
 int main(int argc, char const *argv[]) {
@@ -21,22 +21,21 @@ int main(int argc, char const *argv[]) {
 	cin >> k;
 	while(k--) {
 		cin >> x >> y;
-		if(area[x][y] >= 1) area[x][y] = 3;
+		int count = 0;
+		if(area[x][y] >= 1) {
+			area[x][y] = 0;
+			count++; 
+		} 
 		for(int i = 0; i < 4; i++) {
 			int newX = x + X[i];
 			int newY = y + Y[i];
 			if(area[newX][newY] > 0 && !outofrange(newX, newY)) {
 				area[newX][newY]++;
-			}
-		}
-		int count = 0;
-		for(int i = 0; i < m; i++) {
-			for(int j = 0; j < n; j++) {
-				if(area[i][j] == 3) {
+				if(area[newX][newY] == 3) {
+					area[newX][newY] = 0;
 					count++;
-					area[i][j] = 0;
 				}
-			}
+			} 
 		}
 		cout << count << endl;
 	}
