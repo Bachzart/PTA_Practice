552fbc6f9cb9d453cf3cd2c01738eb00b897425a
Bachzart <whistlesilp@gmail.com>
2019-09-12 14:57:50 +0800

    refactor: simplified

--- 201909_MOOC_DS_ZhouQiang/P3_c++.cpp
+++ 201909_MOOC_DS_ZhouQiang/P3_c++.cpp
@@ -2,46 +2,43 @@
 using namespace std;
 int n, board[25][25] = {0};
 bool judge(int i, int j, int &x, int &y) {
-	// upright
 	bool flag1, flag2, flag3, flag4;
-	if(i - 2 < 0 || i + 2 > n - 1) flag1 = false;
-	else {
-		if(board[i - 1][j] == -1 && board[i - 2][j] == -1 && 
-		board[i + 1][j] == -1 && board[i + 2][j] == -1) {
+	flag1 = flag2 = flag3 = flag4 = false;
+	// upright
+	if(!(i - 2 < 0 || i + 2 > n - 1)) {
+		if(board[i - 1][j] == -1 && board[i - 2][j] == -1 &&
+		    board[i + 1][j] == -1 && board[i + 2][j] == -1) {
 			x = j, y = i - 2;
 			flag1 = true;
 		}
 	}
 	// lateral
-	if(j - 2 < 0 || j + 2 > n - 1) flag2 = false;
-	else {
-		if(board[i][j - 1] == -1 && board[i][j - 2] == -1 && 
-		board[i][j + 1] == -1 && board[i][j + 2] == -1) {
+	if(!(j - 2 < 0 || j + 2 > n - 1)) {
+		if(board[i][j - 1] == -1 && board[i][j - 2] == -1 &&
+		    board[i][j + 1] == -1 && board[i][j + 2] == -1) {
 			x = j - 2, y = i;
 			flag2 = true;
 		}
 	}
-	// left diagonal 
-	if(i - 2 < 0 || i + 2 > n - 1 || j - 2 < 0 || j + 2 > n - 1) flag3 = false;
-	else {
-		if(board[i - 2][j - 2] == -1 && board[i - 1][j - 1] == -1 && 
-		board[i + 1][j + 1] == -1 && board[i + 2][j + 2] == -1) {
+	// left diagonal
+	if(!(i - 2 < 0 || i + 2 > n - 1 || j - 2 < 0 || j + 2 > n - 1)) {
+		if(board[i - 2][j - 2] == -1 && board[i - 1][j - 1] == -1 &&
+			board[i + 1][j + 1] == -1 && board[i + 2][j + 2] == -1) {
 			x = j - 2, y = i - 2;
 			flag3 = true;
 		}
 	}
 	// right diagonal
-	if(i - 2 < 0 || i + 2 > n - 1 || j - 2 < 0 || j + 2 > n - 1) flag4 = false;
-	else {
-		if(board[i - 2][j + 2] == -1 && board[i - 1][j + 1] == -1 && 
-		board[i + 1][j - 1] == -1 && board[i + 2][j - 2] == -1) {
+	if(!(i - 2 < 0 || i + 2 > n - 1 || j - 2 < 0 || j + 2 > n - 1)) {
+		if(board[i - 2][j + 2] == -1 && board[i - 1][j + 1] == -1 &&
+		    board[i + 1][j - 1] == -1 && board[i + 2][j - 2] == -1) {
 			x = j - 2, y = i + 2;
 			flag4 = true;
 		}
 	}
 	if(flag1 || flag2 || flag3 || flag4) return true;
 	else return false;
-} 
+}
 int main() {
 	cin >> n;
 	char temp;
@@ -51,13 +48,11 @@ int main() {
 			if(temp == '*') board[i][j] = -1;
 		}
 	}
-	int flag = false, x, y; 
+	int flag = false, x, y;
 	for(int i = 0; i < n; i++) {
 		for(int j = 0; j < n; j++) {
-			if(board[i][j] == -1){
-				flag = judge(i, j, x, y);
-				if(flag) break;
-			}
+			if(board[i][j] == -1) flag = judge(i, j, x, y);
+			if(flag) break;
 		}
 		if(flag) break;
 	}
