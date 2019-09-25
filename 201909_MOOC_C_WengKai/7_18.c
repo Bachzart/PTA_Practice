9b7649c96b0c5140117c1b9ed2f9b790355a43c7
Bachzart <whistlesilp@gmail.com>
2019-09-25 15:42:45 +0800

    new problem

--- 201909_MOOC_C_WengKai/7_18.c
+++ 201909_MOOC_C_WengKai/7_18.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
 int main() {
-	
-	
+	double journey, fee = 0;
+	int time;
+	scanf("%lf %d", &journey, &time);
+	if(journey <= 3) fee = 10.0;
+	else if(journey <= 10) fee = 10.0 + (journey - 3.0) * 2.0;
+	else fee = 24.0 + (journey - 10.0) * 3.0;
+	if(time >= 5) fee += time / 5 * 2;
+	printf("%.0lf", fee);
 	return 0;
 }
