daabdc1bd799063d716227a83fa224d2659c8c03
Bachzart <whistlesilp@gmail.com>
2019-10-28 15:28:01 +0800

    new solution

--- 201909_MOOC_DS_ZhouQiang/P12_c++.cpp
+++ 201909_MOOC_DS_ZhouQiang/P12_c++.cpp
@@ -1,17 +1,34 @@
 #include <iostream>
+#include <queue>
+#include <cstring>
 using namespace std;
 const int maxn = 20 + 5;
 int G[maxn][maxn], n;
-bool isvisit[maxn];
+bool isvisit[maxn] = {false};
 void dfs(int node, int &num) {
 	isvisit[node] = true;
 	num++;
 	for(int i = 0; i < n; i++) {
-		if(!isvisit[i] && G[node][i] == 1) dfs(i, num);
+		if(!isvisit[i] && G[node][i]) dfs(i, num);
 	}
 }
-void init() {
-	for(int i = 0; i < maxn; i++) isvisit[i] = false;
+int bfs(int node) {
+	queue<int> q;
+	q.push(node);
+	isvisit[node] = true;
+	int num = 1;
+	while(!q.empty()) {
+		int front = q.front();
+		q.pop();
+		for(int i = 0; i < n; i++) {
+			if(!isvisit[i] && G[front][i]) {
+				num++;
+				q.push(i);
+				isvisit[i] = true;
+			}
+		}
+	}
+	return num;
 }
 int main() {
 	cin >> n;
@@ -20,15 +37,14 @@ int main() {
 			cin >> G[i][j]; 
 		}
 	}
-	int v1, v2, num1, num2;
+	int v1, v2, num1 = 0, num2 = 0;
 	cin >> v1 >> v2;
-	num1 = num2 = 0;
-	init();
-	dfs(v1, num1);
-	init();
-	dfs(v2, num2);
+//	dfs(v1, num1);
+	num1 = bfs(v1);
+	memset(isvisit, 0, sizeof(isvisit));
+//	dfs(v2, num2);
+	num2 = bfs(v2);
 	cout << num1 << endl << num2 << endl;
-	if(isvisit[v1]) cout << "Yes";
-	else cout << "No";
+	isvisit[v1] ? cout << "Yes" : cout << "No";
 	return 0;
 } 
