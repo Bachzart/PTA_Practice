94aeb6f7feb133d37cd642a23d87e9afc6c88bb5
Bachzart <whistlesilp@gmail.com>
2019-11-01 22:47:15 +0800

    add new solution

--- 201909_MOOC_DS_ChenYue/03_3.cpp
+++ 201909_MOOC_DS_ChenYue/03_3.cpp
@@ -1,3 +1,50 @@
+/* method 1: Do not build a tree. use recursion */
+#include <iostream>
+#include <stack>
+using namespace std;
+const int maxn = 30 + 5; 
+int in[maxn], pre[maxn], post[maxn], n;
+void solve(int preL, int inL, int postL, int n) {
+	if(n == 0) return;
+	if(n == 1) {
+		post[postL] = pre[preL];
+		return;
+	}
+	int root = pre[preL], i;
+	post[postL + n - 1] = root;
+	for(i = 0; i < n; i++) {
+		if(in[inL + i] == root) break;
+	}
+	int L = i, R = n - L - 1;
+	solve(preL + 1, inL, postL, L);
+	solve(preL + L + 1, inL + L + 1, postL + L, R);
+}
+int main() {
+	string ope;
+	int node, cnt1 = 0, cnt2 = 0;
+	cin >> n;
+	stack<int> st;
+	for(int i = 0; i < 2 * n; i++) {
+		cin >> ope;
+		if(ope == "Push") {
+			cin >> node;
+			pre[cnt1++] = node;
+			st.push(node);
+		} else if(ope == "Pop") {
+			in[cnt2++] = st.top();
+			st.pop();
+		}
+	}
+	solve(0, 0, 0, n);
+	for(int i = 0; i < n; i++) {
+		cout << post[i];
+		if(i != n - 1) putchar(' ');
+	}
+	return 0;
+}
+
+/* method 2: use static tree 
+ 
 #include <iostream>
 #include <string>
 #include <stack>
@@ -55,3 +102,5 @@ int main() {
 	postorder(0);
 	return 0;
 }
+
+*/
