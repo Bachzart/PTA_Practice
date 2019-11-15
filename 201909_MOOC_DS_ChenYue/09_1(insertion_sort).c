31b3410a28ac494fd50fb3cdb8fadb895b8697bb
Bachzart <whistlesilp@gmail.com>
2019-11-15 17:07:51 +0800

    insertion sort

---
+++ 201909_MOOC_DS_ChenYue/09_1(insertion_sort).c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#define maxn 100005
+int array[maxn], n;
+void insertion_sort(int *array, int n) {
+	for(int i = 1; i < n; i++) {
+		int tmp = array[i], j;
+		for(j = i; j > 0 && tmp < array[j - 1]; j--) {
+			array[j] = array[j - 1];
+		}
+		array[j] = tmp;
+	}
+}
+int main() {
+	scanf("%d", &n);
+	for(int i = 0; i < n; i++) {
+		scanf("%d", array + i);
+	}
+	insertion_sort(array, n);
+	for(int i = 0; i < n; i++) {
+		printf("%d", array[i]);
+		if(i < n - 1) putchar(' ');
+	}
+	return 0;
+} 
+
+/*
+samples:
+in:
+11
+4 981 10 -17 0 -20 29 50 8 43 -5
+out:
+
+*/
