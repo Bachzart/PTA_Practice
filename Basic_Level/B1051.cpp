f6dd6ce9f681d8ad5b4f43bbbebb64a038628d8c
bachzart <whistlesilp@gmail.com>
2022-03-01 19:45:46 +0800

    add notes & simplification

--- Basic_Level/B1051.cpp
+++ Basic_Level/B1051.cpp
@@ -6,17 +6,12 @@ int main(int argc, char const *argv[]) {
 	scanf("%lf %lf %lf %lf", &R1, &P1, &R2, &P2);
 	R = R1 * R2 * (cos(P1) * cos(P2) - sin(P1) * sin(P2));
 	P = R1 * R2 * (cos(P1) * sin(P2) + sin(P1) * cos(P2));
-	if(-0.05 < R && R < 0.05) {
-		R = 0.0;
-	}
-	if(-0.05 < P && P < 0.05) {
-		P = 0.0;
-	}
+	// prevent to print "-0.00"
+	if(-0.01 < R && R < 0) R = 0.0;
+	if(-0.01 < P && P < 0) P = 0.0;
 	printf("%.2lf", R);
-	if(P > 0) {
+	if(P >= 0) {
 		printf("+%.2lfi\n", P);
-	} else if (P == 0) {
-		printf("+0.00i\n");
 	} else {
 		printf("-%.2lfi\n", fabs(P));
 	}
