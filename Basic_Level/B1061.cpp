bd06e4b716715ca1be9e4c878dc6fe4b75a79cbb
bachzart <whistlesilp@gmail.com>
2022-03-03 18:45:37 +0800

    refactor

--- Basic_Level/B1061.cpp
+++ Basic_Level/B1061.cpp
@@ -3,7 +3,7 @@
 int main(int argc, char const *argv[]) { 
 	int N, M;
 	scanf("%d %d", &N, &M);
-	int i, j, Scores[M], Right[M], Temp[M], Sum;
+	int i, j, Scores[M], Right[M], Temp, Sum;
 	for(i = 0; i < M; i++) {
 		scanf("%d", &Scores[i]);
 	}
@@ -13,10 +13,8 @@ int main(int argc, char const *argv[]) {
 	while(N--) {
 		Sum = 0;
 		for(i = 0; i < M; i++) {
-			scanf("%d", &Temp[i]);
-			if(Temp[i] == Right[i]) {
-				Sum += Scores[i];
-			}
+			scanf("%d", &Temp);
+			if(Temp == Right[i]) Sum += Scores[i];
 		}
 		printf("%d\n", Sum);
 	}
