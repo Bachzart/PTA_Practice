1c41e7bd490d0d5001199602626a535e65b66d0f
bachzart <whistlesilp@gmail.com>
2022-03-05 11:47:25 +0800

    simplification

--- Basic_Level/B1071.cpp
+++ Basic_Level/B1071.cpp
@@ -1,31 +1,27 @@
-#include <stdio.h>
-#define MAXK 100
+#include <cstdio>
 
 int main(int argc, char const *argv[]) {
-	int T, K, n1, b, t, n2, x, system_result;
+	int T, K, n1, b, t, n2, remains;
 	scanf("%d %d", &T, &K);
-	x = T;
-	while(K-- && x) {
+	remains = T;
+	while(K--) {
 		scanf("%d %d %d %d", &n1, &b, &t, &n2);
-		if(t > x) {
-			printf("Not enough tokens.  Total = %d.\n", x);
+		if(remains <= 0) {
+			printf("Game Over.\n");
+			break;
+		}
+		if(t > remains) {
+			printf("Not enough tokens.  Total = %d.\n", remains);
 			continue;
 		}
-		if(n2 > n1) {
-			system_result = 1;
+		if((n1 > n2 && b == 0) || (n1 < n2 && b == 1)) {
+			remains += t;
+			printf("Win %d!  ", t);
 		} else {
-			system_result = 0;
+			remains -= t;
+			printf("Lose %d.  ", t);
 		}
-		if(system_result == b) {
-			x += t;
-			printf("Win %d!  Total = %d.\n", t, x);
-		} else {
-			x -= t;
-			printf("Lose %d.  Total = %d.\n", t, x);
-		}
-	}
-	if(x <= 0) {
-		printf("Game Over.\n");
+		printf("Total = %d.\n", remains);
 	}
 	return 0;
 }
