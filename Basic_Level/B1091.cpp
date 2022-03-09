6c6b8b6d4f95506dcfecbc734048f841c2e7ee80
bachzart <whistlesilp@gmail.com>
2022-03-09 22:19:09 +0800

    simplification

--- Basic_Level/B1091.cpp
+++ Basic_Level/B1091.cpp
@@ -1,8 +1,36 @@
+// version 1
+#include <cstdio>
+
+int main() {
+	int n, k, m, digit, tmp;
+	scanf("%d", &m);
+	while(m--) {
+		scanf("%d", &k);
+		tmp = k, digit = 1;
+		while(tmp != 0) {
+			tmp /= 10;
+			digit *= 10;
+		}
+		bool flag = false;
+		for(n = 1; n < 10; n++) {
+			tmp = n * k * k;
+			if(tmp % digit == k) {
+				flag = true;
+				break;
+			}
+		}
+		if(flag) printf("%d %d\n", n, tmp);
+		else printf("No\n");
+	}
+	return 0;
+}
+
+
+/* 
+// version 2
 #include <stdio.h>
 #include <stdbool.h>
-
 bool Judge_Automorphic(int test_number, int original_number);
-
 
 int main(int argc, char const *argv[]) {
 	int M, N, K, flag, each_item;
@@ -41,3 +69,4 @@ bool Judge_Automorphic(int test_number, int original_number) {
 	}
 	return flag;
 }
+*/
