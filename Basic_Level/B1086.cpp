1c07122c3ea12c3280344ff5eeb5afcbdf79ee0d
bachzart <whistlesilp@gmail.com>
2022-03-09 17:06:08 +0800

    new solutions

--- Basic_Level/B1086.cpp
+++ Basic_Level/B1086.cpp
@@ -1,3 +1,4 @@
+// way 1
 #include <stdio.h>
 
 int main(int argc, char const *argv[]) {
@@ -13,3 +14,26 @@ int main(int argc, char const *argv[]) {
 	printf("%d\n", result);
 	return 0;
 } 
+
+/*
+// way 2
+
+#include <cstdio>
+char ans[10];
+
+int main() {
+	int A, B, C, len = 0;
+	scanf("%d %d", &A, &B);
+	C = A * B;
+	do{
+		ans[len++] = C % 10 + '0';
+		C /= 10;
+	} while(C != 0);
+	ans[len] = '\0';
+	char *p = ans;
+	while(*p == '0') p++;
+	puts(p);
+	return 0;
+}
+
+*/
