2d7c3781bfcfb5a2c0bebe48542a40d7f74584c7
bachzart <whistlesilp@gmail.com>
2022-03-20 15:58:17 +0800

    simplification

--- Silver_Level_Test2/7-4.cpp
+++ Silver_Level_Test2/7-4.cpp
@@ -1,17 +1,12 @@
-#include <iostream>
-using namespace std;
+#include <cstdio>
 
 int main() {
-	int pa, pb, a, b, c, ta, tb;
-	ta = tb = 0;
+	int pa, pb, a, b, c, ta = 0;
 	scanf("%d %d\n%d %d %d", &pa, &pb, &a, &b, &c);
-	if(a) tb += 1;
-	else ta += 1;
-	if(b) tb += 1;
-	else ta += 1;
-	if(c) tb += 1;
-	else ta += 1;
+	if(!a) ta += 1;
+	if(!b) ta += 1;
+	if(!c) ta += 1;
 	if(ta == 3 || (pa > pb && ta >= 1)) printf("The winner is a: %d + %d\n", pa, ta);
-	else printf("The winner is b: %d + %d\n", pb, tb);
+	else printf("The winner is b: %d + %d\n", pb, 3 - ta);
 	return 0;
 }
