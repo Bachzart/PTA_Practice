4d42bbec1e5cb961eee799a257f772e016a1988b
bachzart <whistlesilp@gmail.com>
2022-05-11 21:46:56 +0800

    add new solutions

--- Advanced_Level/A1028.cpp
+++ Advanced_Level/A1028.cpp
@@ -35,3 +35,39 @@ bool cmp(student a, student b) {
 		else return strcmp(a.id, b.id) < 0;
 	}
 }
+
+/* C++ */
+#include <iostream>
+#include <algorithm>
+using namespace std;
+const int maxn = 100000 + 5;
+struct student {
+	string id, name;
+	int grade;
+} stu[maxn];
+bool cmp1(student a, student b) {
+	if(a.name != b.name) return a.name < b.name;
+	else return a.id < b.id;
+}
+int n, c;
+bool cmp(student a, student b) {
+	if(c == 1) return a.id < b.id;
+	else if(c ==2) {
+		if(a.name != b.name) return a.name < b.name;
+		else return a.id < b.id;
+	} else {
+		if(a.grade != b.grade) return a.grade < b.grade;
+		else return a.id < b.id;
+	}
+}
+int main() {
+	cin >> n >> c;
+	for(int i = 0; i < n; i++) {
+		cin >> stu[i].id >> stu[i].name >> stu[i].grade;
+	}
+	sort(stu, stu + n, cmp);
+	for(int i = 0; i < n; i++) {
+		cout << stu[i].id << ' ' << stu[i].name << ' ' << stu[i].grade << endl;
+	}
+	return 0;
+}
--- Advanced_Level/A1055.cpp
+++ Advanced_Level/A1055.cpp
@@ -49,3 +49,48 @@ bool cmp(people a, people b) {
 	else if(a.age != b.age) return a.age < b.age;
 	else return strcmp(a.name, b.name) < 0;
 }
+
+/* C++ */
+#include <iostream>
+#include <algorithm>
+using namespace std;
+const int maxn = 100000 + 5;
+struct people {
+	string name;
+	int age, worth;
+} peo[maxn], output[maxn]; 
+int Age[maxn] = {0};
+bool cmp(people a, people b) {
+	if(a.worth != b.worth) return a.worth > b.worth;
+	else if(a.age != b.age) return a.age < b.age;
+	else return a.name < b.name;
+}
+int main() {
+	int n, k;
+	cin >> n >> k;
+	for(int i = 0; i < n; i++) {
+		cin >> peo[i].name >> peo[i].age >> peo[i].worth;
+	}
+	sort(peo, peo + n, cmp);
+	int index = 0;
+	for(int i = 0; i < n; i++) {
+		if(Age[peo[i].age] < 100) {
+			Age[peo[i].age]++;
+			output[index++] = peo[i];
+		}
+	}
+	int M, amin, amax;
+	for(int i = 1; i <= k; i++) {
+		cout << "Case #" << i << ":" << endl;
+		cin >> M >> amin >> amax;
+		int cnt = M;
+		for(int i = 0; i < index; i++) {
+			if(amin <= output[i].age && output[i].age <= amax) {
+				cout << output[i].name << ' ' << output[i].age << ' ' << output[i].worth << endl;
+				if(--cnt == 0) break;
+			}
+		}
+		if(cnt == M) cout << "None" << endl;
+	}
+	return 0;
+}
