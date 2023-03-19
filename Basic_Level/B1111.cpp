fa2ed08246c7487b3328c3702157e18b364229d3
bachzart <whistlesilp@gmail.com>
2023-03-19 18:03:22 +0800

    add new solution:sparkles:

--- Basic_Level/B1111.cpp
+++ Basic_Level/B1111.cpp
@@ -1,3 +1,54 @@
+// method 1: use int
+#include <cstdio>
+#include <algorithm>
+#include <string>
+#include <unordered_map>
+using namespace std;
+unordered_map<string, int> mon2num = {
+	{"Jan", 1},
+	{"Feb", 2},
+	{"Mar", 3},
+	{"Apr", 4},
+	{"May", 5},
+	{"Jun", 6},
+	{"Jul", 7},
+	{"Aug", 8},
+	{"Sep", 9},
+	{"Oct", 10},
+	{"Nov", 11},
+	{"Dec", 12},
+};
+
+int main(int argc, char const *argv[]) {
+	char month[5];
+	int day, year, N;
+	scanf("%d", &N);
+	while(N--) {
+		scanf("%s %d, %d", month, &day, &year);
+		string str_mon = month;
+		int mon = mon2num[month];
+		int n = year * 10000 + mon * 100 + day;
+		string ans = to_string(n), rans;
+		if(ans.length() == 7)
+			ans = "0" + ans;
+		if(ans.length() == 6)
+			ans = "00" + ans;
+		if(ans.length() == 5)
+			ans = "000" + ans;
+		rans = ans;
+		reverse(rans.begin(), rans.end());
+		if(ans == rans)
+			printf("Y ");
+		else
+			printf("N ");
+		printf("%s\n", ans.c_str());
+	}
+	return 0;
+}
+
+/*
+// method 2: use string
+
 #include <cstdio>
 #include <algorithm>
 #include <string>
@@ -44,3 +95,4 @@ int main(int argc, char const *argv[]) {
 	}
 	return 0;
 }
+*/
